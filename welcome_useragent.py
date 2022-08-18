319db3d02ae47d32799bd899170c9faf1a8b30a3
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2022-08-18 11:55:25 +0200

    Added user-agent info to the app

--- welcome_useragent.py
+++ welcome_useragent.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from user_agents import parse
 import requests
 
 app = Flask(__name__)
@@ -6,9 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     msg = "Welcome 2022"
+    
     url = "https://www.google.com"
     response = requests.get(url)
-    return msg + '\n' + str(response.headers)
-
+    
+    ua_string = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+    user_agent = parse(ua_string)
+    user_agent_info = str(user_agent.browser) + '<br/> <br/>' + str(user_agent.os)
+    
+    return msg + '<br/> <br/>' + user_agent_info + '<br/> <br/>' + str(response.headers)
 
 app.run(host='0.0.0.0')
